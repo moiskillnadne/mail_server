388d4e9501e16e976394f421cef6e94b0c6176b3
Vardan Mkhitaryan <vmkhitaryan@scnsoft.com>
2024-09-20 18:56:53 +0400

    Typescript,eslint,dotenv,prettier configuration

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
-const PORT = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world");
+const PORT = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world');
 });
 
-app.get("/healthcheck", (req: Request, res: Response) => {
-  res.status(200).json("OK");
+app.get('/healthcheck', (req: Request, res: Response) => {
+  res.status(200).json('OK');
 });
 
-app.get("*", function (req, res) {
-  res.status(404).send("Page not found");
+app.get('*', function (req, res) {
+  res.status(404).send('Page not found');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started successfully`);
+  console.log(`Server started successfully on port ${PORT}`);
 });
